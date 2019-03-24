863e9a657a93060422fb226294e1c7ace9bc5480
JohnB64 <47114060+JohnB64@users.noreply.github.com>
2019-03-24 18:32:41 -0400

    Exercise 6.71
    Created the BoxBall class and filled out the directives.

---
+++ BoxBall.java
@@ -0,0 +1,33 @@
+
+/**
+ * Creates a rectangle with balls inside that randomly bounce off the sides.
+ *
+ * @author John Burkert
+ * @version March 24th 2019
+ */
+public class BoxBall
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class BoxBall
+     */
+    public BoxBall()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
